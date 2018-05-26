62c69dab75335ec9eac87f111eacf7172e4077e8
Danilo F. Silva <danilofes@gmail.com>
2018-05-26 00:07:44 -0300

    Simulação de chamadas async

---
+++ blueprint-ts/src/backend.ts
@@ -0,0 +1,29 @@
+
+
+export const eventos = {
+  busca: () => {
+    return delayed<MeusEventosDto>({
+      eventos: [
+        { codigo: 1, nome: "Audiência pública da comissão de Saúde" },
+        { codigo: 2, nome: "A vaquinha LeLê" }
+      ]
+    });
+  },
+
+  obtem: (codigo: number) => {
+    return delayed<EventoDto>({ codigo: 1, nome: "Audiência pública da comissão de Saúde" });
+  }
+};
+
+export interface MeusEventosDto {
+  eventos: EventoDto[];
+}
+
+export interface EventoDto {
+  codigo: number,
+  nome: string
+}
+
+function delayed<T>(valor: T): Promise<T> {
+  return new Promise(resolve => setTimeout(() => resolve(valor), 2000));
+}
